233383656b8746158fa2b8039bc24349bed4140a
Andrey Dolgushin <mailer.rand@gmail.com>
2021-01-14 18:49:23 +0500

    Homework for fourth lesson of Android Base course

--- app/src/main/java/me/rubl/gbandroidbase/CitiesActivity.java
+++ app/src/main/java/me/rubl/gbandroidbase/CitiesActivity.java
@@ -1,5 +1,6 @@
 package me.rubl.gbandroidbase;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
@@ -13,16 +14,17 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 
-public class CitiesActivity extends AppCompatActivity {
+import me.rubl.gbandroidbase.entity.City;
+
+public class CitiesActivity extends AppCompatActivity implements OnItemClickCityRecyclerListener {
 
     Toolbar toolbar;
     RecyclerView rvCities;
     EditText etSearchCityName;
 
     CitiesAdapter citiesAdapter;
-    ArrayList<String> citiesList;
+    ArrayList<City> citiesList;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -36,8 +38,8 @@ public class CitiesActivity extends AppCompatActivity {
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         getSupportActionBar().setDisplayShowHomeEnabled(true);
 
-        citiesList = new ArrayList<>(Arrays.asList(getResources().getStringArray(R.array.cities)));
-        citiesAdapter = new CitiesAdapter(citiesList);
+        citiesList = City.getMockCities();
+        citiesAdapter = new CitiesAdapter(this, citiesList, this);
 
         LinearLayoutManager llManager = new LinearLayoutManager(this, RecyclerView.VERTICAL, false);
         rvCities.setLayoutManager(llManager);
@@ -63,10 +65,11 @@ public class CitiesActivity extends AppCompatActivity {
     }
 
     private void filter(String text) {
-        ArrayList<String> filteredList = new ArrayList<>();
+        ArrayList<City> filteredList = new ArrayList<>();
 
-        for (String city : citiesList) {
-            if (city.toLowerCase().contains(text.toLowerCase())) {
+        for (City city : citiesList) {
+            String cityName = this.getResources().getString(city.getNameResourceId());
+            if (cityName.toLowerCase().contains(text.toLowerCase())) {
                 filteredList.add(city);
             }
         }
@@ -89,5 +92,13 @@ public class CitiesActivity extends AppCompatActivity {
     public boolean onSupportNavigateUp() {
         onBackPressed();
         return true;
+    }
+
+    @Override
+    public void onItemClick(City city) {
+        Intent result = new Intent();
+        result.putExtra(City.ARGUMENT_KEY, city);
+        setResult(RESULT_OK, result);
+        finish();
     }
 }
---
+++ app/src/main/java/me/rubl/gbandroidbase/OnItemClickCityRecyclerListener.java
@@ -0,0 +1,9 @@
+package me.rubl.gbandroidbase;
+
+import android.view.View;
+
+import me.rubl.gbandroidbase.entity.City;
+
+public interface OnItemClickCityRecyclerListener{
+    void onItemClick(City city);
+}
